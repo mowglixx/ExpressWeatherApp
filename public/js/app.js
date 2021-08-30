1d1bf434d12b8f4f415a7887440927c3d5218dea
Daniel Monaghan <mowglixx@users.noreply.github.com>
2021-08-30 22:38:00 +0100

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,6 @@
 // Client Side Java script!!!
 console.log('Client Side Javascript Loaded')
 
-async function fetchAsync (url, callback) {
-    let response = await fetch(url);
-    let data = await response.json();
-    //console.log(data)
-    callback(data)
-}
-
-//const oof = document.getElementById('json')
-
-
 fetch('/weather/Manchester').then((response) => {
     response.json().then(({weather,location}) => {
         replacementString = 'The weather in '+ 
